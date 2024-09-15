6730091f243b9f9822cca307f113494e71b8d03e
vedarshshah <33162616+vedarshshah@users.noreply.github.com>
2024-09-15 21:29:53 +0000

    Cleaned up code from day 3

--- turtlesim_ws/src/my_turtlesim/my_turtlesim/move_turtle.py
+++ turtlesim_ws/src/my_turtlesim/my_turtlesim/move_turtle.py
@@ -1,14 +1,20 @@
+# Import Python standard libraries
 import sys
 import math
 
+# Import ROS2 Python API
 import rclpy
 from rclpy.node import Node
 
+# Import message definitions
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
+# Turtle will stop rotating in place when its angle is within this threshold of the desired angle. Units: Radians
 ANGULAR_THRESHOLD = 0.001
-ANGULAR_GAIN = 1
+
+# Constant multiplier for angular velocity when turtle is rotating in place
+ANGULAR_GAIN = 1.0
 
 class MoveTurtle(Node):
     def __init__(self):
@@ -17,10 +23,9 @@ class MoveTurtle(Node):
         # Initialize publisher for turtle velocity
         self.vel_publisher = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
 
+        # Initialize subscriber for turtle pose
         self.turtle_pose = None
         self.pose_subscriber = self.create_subscription(Pose, 'turtle1/pose', self.got_turtle_pose, 10)
-
-        self.rate = self.create_rate(20)
 
     def got_turtle_pose(self, turtle_pose: Pose):
         """
@@ -31,25 +36,19 @@ class MoveTurtle(Node):
         """
         self.turtle_pose = turtle_pose
 
-        # Logging
-        # print("message")
-        # self.get_logger().info(str(self.turtle_pose))
-
     def run(self):
-        # Publish velocity to move turtle forward
-        # rclpy.spin(self)
-        # twist = Twist()
-        # twist.linear.x = 1.0
+        """
+        Function that runs this node and calls task planning.
+        """
 
-        # while rclpy.ok():
-        #     self.vel_publisher.publish(twist)
-        #     rclpy.spin_once(self)
-        #     self.rate.sleep()
-
+        # Wait until turtle pose is received
         while self.turtle_pose is None:
             rclpy.spin_once(self)
 
-        self.rotate_to_global_theta(-math.pi)
+        # Test `rotate_to_global_theta`
+        thetas = [0, math.pi/2, math.pi, -math.pi/2, 0, -math.pi/2, -math.pi, math.pi/2, 0]
+        for theta in thetas:
+            self.rotate_to_global_theta(theta)
 
     """
     Task #1: Draw Square
@@ -75,11 +74,10 @@ class MoveTurtle(Node):
         """
 
         """
-        What is the high-level algorithm to move to an x,y position?
-
         Need: Turtle's pose (x, y, theta)
-        Need to subscribe to /turtle1/pose
+        Need to subscribe to /turtle1/pose (DONE)
 
+        Algorithm:
         1. Compute theta that turtle needs to be at to face (x, y)
         2. Rotate to computed theta
         3. Set velocity to positive linear x to move forward until turtle reaches (x, y)
@@ -93,41 +91,31 @@ class MoveTurtle(Node):
         Args:
             theta (float): The angle to rotate the turtle to. Must be in radians.
         """
-
-        # theta could be any value (not nececessarily betwen -pi and pi)
-
-        """
-        1. Compute delta_theta (the angle that we need to rotate from the current position)
-        2. Loop while delta_theta > threshold:
-            1. Publish angular Z velocity to rotate turtle
-            2. Update the detla_theta
-        3. Stop turtle by publishing zero velocity
-        """
-
         # Compute amount to rotate
-        delta_theta = self.convert_0_2pi_to_neg_pi_to_pi(theta - self.turtle_pose.theta)
+        delta_theta = self.convert_0_2pi_to_neg_pi_pi(theta - self.turtle_pose.theta)
 
         twist = Twist()
 
         # Loop while turtle isn't within a threshold of the desired theta
         while rclpy.ok() and abs(delta_theta) > ANGULAR_THRESHOLD:
+
+            # Update angular velocity to be proportional to amount left to rotate
             twist.angular.z = delta_theta * ANGULAR_GAIN
 
             # Publish velocity
             self.vel_publisher.publish(twist)
 
+            # Allow ROS to process published message
             rclpy.spin_once(self)
 
             # Update remaining angle
-            delta_theta = self.convert_0_2pi_to_neg_pi_to_pi(theta - self.turtle_pose.theta)
-
-            self.rate.sleep()
+            delta_theta = self.convert_0_2pi_to_neg_pi_pi(theta - self.turtle_pose.theta)
 
         # Stop turtle
         self.vel_publisher.publish(Twist())
 
 
-    def convert_0_2pi_to_neg_pi_to_pi(self, theta: float):
+    def convert_0_2pi_to_neg_pi_pi(self, theta: float):
         """
         Convert an angle to range [-pi, pi].
 
